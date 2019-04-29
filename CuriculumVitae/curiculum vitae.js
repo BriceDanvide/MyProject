51352e879bb47823143588290d3e08b86edbf1d0
BriceDanvide <Sedamihou@gmail.com>
2019-04-29 18:17:38 +0200

    Edit curiculum vitae.js creation map

--- CuriculumVitae/curiculum vitae.js
+++ CuriculumVitae/curiculum vitae.js
@@ -1,55 +1,54 @@
 
-const myDate= document.querySelector('#myDate');
+/*const myDate= document.querySelector('#myDate');
 console.log(myDate);
 myDate.innerText = new Date().toLocaleDateString();
 
-
+*/
 
 //declaration des objets
 
-const coordonnee={
-
-    firstName:'Brice',
-    lastName :'Danvidé',
-    gender: 'M',
-    adresse:'Paris',
-    email: 'brice_danvide@hotmail.fr',
-    tel :'0641664106',
-
-
-};
-
-const experience = {
-
-    EntrepriseName1:'Microsoft France Mars 2009 /2018',
-    EntrepriseName2:'Sun Microsystems Juin 2007 /2009',
-    JobTitle1:'Business Development Manager Surface',
-    JobTitle2:'Responsable Sponsoring TECHDAYS 2015',
-    JobTitle3:'Channel Development Manager SMS&P/SMB',
-    JobTitle4:'Technical Sales Specialiste SMS&P /CTM CAM',
-    JobTitle5:'Inside Territory Manager SMS&P / CTM Sud-Ouest',
-    JobTitle6:'Commercial Sédentaire Secteur Bancaire',
-
-    };
-
-const competence=[
-    'Html', 'Css3','Javascript','React Js', 'Angular 6', 'Nodes Js','GitHub', 'Adobe Creative Suite', 'Anglais Professionnel'
-
-
-];
-
-const formation ={
-
-    formationTitle1:'',
-    formationTitle2:'',
-    formationTitle3:'',
-    formationDescription1:'',
-    formationDescription2:'',
-    formationDescription3:'',
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+import {me}from'./data.js'
+
+
+
+
+me.forEach((value,key) =>{
+
+    document.querySelector(key).textContent= value;
+
+   });
+
+
+
+
+
+
+/*console.log(me.get('#jobTitle5'));*/
+
+
+
+
+
+
+
+
+
+
+
+
 
 //creation des fonctions
 
@@ -58,6 +57,19 @@ const formation ={
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 function sayGender(personParameter){
 
@@ -77,33 +89,9 @@ function sayGender(personParameter){
 };
 
 
-
-
 const titre = document.querySelector('#titree');
-console.log(titre);
+
 titre.innerText = sayGender(brice);
 
 
-
-const nom=document.querySelector('#nomComplet');
-console.log (nom);
-
-nom.textContent = brice.lastName+' '+brice.firstName;
-
-
-const adresse=document.querySelector('#adresse');
-console.log (adresse);
-
-adresse.textContent = brice.adresse;
-
-
-const email=document.querySelector('#email');
-console.log (email);
-
-email.textContent = brice.email;
-
-
-const telephone =document.querySelector('#tel');
-console.log (telephone);
-
-telephone.textContent = brice.tel;
+ */
---
+++ CuriculumVitae/data.js
@@ -0,0 +1,30 @@
+export const me = new Map()
+    .set('#firstName','Brice')
+    .set( '#lastName','Danvidé')
+    .set('#adresse','Paris')
+    .set('#email','brice_danvide@hotmail.fr')
+    .set ('#tel','0641664106')
+    .set('#jobTitle1','BusinessDevelopmentManagerSurface')
+    .set('#jobTitle2','ResponsableSponsoringTECHDAYS2015')
+    .set('#jobTitle3','ChannelDevelopmentManagerSMS&P/SMB')
+    .set('#jobTitle4','TechnicalSalesSpecialisteSMS&P/CTMCAM')
+    .set('#jobTitle5','InsideTerritoryManagerSMS&P/CTMSud-Ouest')
+    .set('#jobTitle6','CommercialSédentaireSecteurBancaire')
+    .set('#competence1','Html')
+    .set('#competence2','Css3')
+    .set('#competence3','Javascript')
+    .set('#competence4','React Js')
+    .set('#competence5','Angular 6')
+    .set('#competence6','Nodes Js')
+    .set('#competence7','GitHub')
+    .set('#competence8','Adobe Creative Suite')
+    .set('#competence9','Anglais Professionnel')
+    .set('#formationTitle1','Formation intensive developpeur front-end creation d application web avec Angular/React')
+    .set('#formationTitle2','Communication visuelle, Design produit, Architecture d intérieur, dessin')
+    .set('#formationTitle3','Options Action et Communication Commerciale')
+
+;
+
+
+
+
