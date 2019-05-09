b2d87df66706775f0afd189602824b944971efcd
BriceDanvide <Sedamihou@gmail.com>
2019-05-09 10:25:42 +0200

    FICHE

--- CuriculumVitae/curiculum vitae.js
+++ CuriculumVitae/curiculum vitae.js
@@ -1,11 +1,4 @@
 
-/*const myDate= document.querySelector('#myDate');
-console.log(myDate);
-myDate.innerText = new Date().toLocaleDateString();
-
-*/
-
-//declaration des objets
 
 
 
@@ -34,64 +27,3 @@ me.forEach((value,key) =>{
 
 
 
-
-
-/*console.log(me.get('#jobTitle5'));*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-//creation des fonctions
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-function sayGender(personParameter){
-
-
-    if (personParameter.gender==='F'){
-        return 'DEVELOPPEUSE WEB FRONT END';
-
-
-    }else if (personParameter.gender==='M'){
-
-        return 'DEVELOPPEUR WEB FRONT END';
-    }
-
-    else {
-        return 'DEVELOPPEUR/EUSE WEB FRONT END';
-    }
-};
-
-
-const titre = document.querySelector('#titree');
-
-titre.innerText = sayGender(brice);
-
-
- */
