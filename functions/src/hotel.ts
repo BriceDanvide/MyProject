01bf6b132839eaaef16e90e8f390de04f3a77c7d
BriceDanvide <Sedamihou@gmail.com>
2019-05-23 19:17:31 +0200

    cv en pug

---
+++ functions/src/hotel.ts
@@ -0,0 +1,34 @@
+//export const hotels
+
+export let hostels = [
+    {
+        id: 1,
+        name: 'hotel rose',
+        roomNumbers: 10,
+        pool: true,
+
+
+    },
+    {
+        id: 2,
+        name: 'hotel ocean',
+        roomNumbers: 15,
+        pool: false,
+
+    },
+    {
+        id: 3,
+        name: 'hotel des Pins',
+        roomNumbers: 7,
+        pool: true,
+
+    },
+
+    {
+        id: 4,
+        name: 'hotel des fleurs',
+        roomNumbers: 7,
+        pool: true,
+
+    }
+];
